798d2a1b764b1696a3d75599df1afcec4f1fc984
westonkd <wdrans@gmail.com>
2021-12-07 21:13:08 -0700

    Begin work on vector

---
+++ testing/expect.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+namespace Testing {
+	template <class T>
+	class Expect {
+		std::string context;
+		T actual;
+
+		void printFailure(std::string hint = "");
+		void printSuccess(std::string details = "");
+
+	public:
+		Expect(T actual, std::string context);
+		void to(std::string description, T expected);
+	};
+
+	template <class T>
+	Expect<T>::Expect(T actual, std::string context) {
+		this->context = context;
+		this->actual = actual;
+	}
+
+	template <class T>
+	void Expect<T>::to(std::string description, T expected) {
+		if (this->actual == expected) {
+			this->printSuccess(description);
+			return;
+		}
+
+		std::stringstream hint;
+		hint << description << " ! Expected: " << expected << " Actual: " << this->actual;
+
+		printFailure(hint.str());
+	}
+
+	template <class T>
+	void Expect<T>::printFailure(std::string hint) {
+		std::stringstream failureMessage;
+		failureMessage << "✗ - " << this->context << " " << hint;
+		std::cout << failureMessage.str() << "\n";
+	}
+
+	template <class T>
+	void Expect<T>::printSuccess(std::string details) {
+		std::stringstream successMessage;
+		successMessage << "✓ - " << this-> context << " " << details;
+		std::cout << successMessage.str() << "\n";
+	}
+}
---
+++ vector/vectorTest.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include "vector.h"
+#include "../testing/expect.h"
+
+
+void defaultConstructor() {
+	Custom::Vector<int> subject;
+
+	Testing::Expect<int>(
+		subject.size(),
+		"with the default constructor"
+	).to("to have size() 0", 0);
+
+	Testing::Expect<int>(
+		subject.capacity(),
+		"with the default constructor"
+	).to("to have capacity() 0", 0);
+}
+
+void capacityConstructor() {
+	Custom::Vector<int> subject(10);
+
+	Testing::Expect<int>(
+		subject.capacity(),
+		"with the capacity constructor of 10"
+	).to("to have the specified capacity", 10);
+
+	Testing::Expect<int>(
+		subject.size(),
+		"with the capacity constructor of 10"
+	).to("to have the size 10", 10);
+}
+
+void fillConstructor() {
+	Custom::Vector<int> subject(10, 1);
+
+	Testing::Expect<int>(
+		subject.size(),
+		"with the fill constructor of 10, 1"
+	).to("to have the size 10", 10);
+
+	Testing::Expect<int>(
+		subject.capacity(),
+		"with the fill constructor of 10, 1"
+	).to("to have the capacity 10", 10);
+
+	Testing::Expect<int>(
+		subject.at(0),
+		"with the fill constructor of 10, 1"
+	).to("to be filled with 1 (start)", 1);
+
+	Testing::Expect<int>(
+		subject.at(9),
+		"with the fill constructor of 10, 1"
+	).to("to be filled with 1 (end)", 1);
+}
+
+void copyConstructor() {
+	Custom::Vector<int> source(10, 1);
+	Custom::Vector<int> subject(source);
+
+	Testing::Expect<int>(
+		subject.size(),
+		"with copy constructor of a vector of size 10 filled with 1"
+	).to("to have the size 10", 10);
+
+	Testing::Expect<int>(
+		subject.capacity(),
+		"with copy constructor of a vector of size 10 filled with 1"
+	).to("to have the capacity 10", 10);
+
+	Testing::Expect<int>(
+		subject.at(0),
+		"with copy constructor of a vector of size 10 filled with 1"
+	).to("to be filled with 1 (start)", 1);
+
+	Testing::Expect<int>(
+		subject.at(9),
+		"with copy constructor of a vector of size 10 filled with 1"
+	).to("to be filled with 1 (end)", 1);
+}
+
+int main() {
+	defaultConstructor();
+	capacityConstructor();
+	fillConstructor();
+	return 0;
+}
